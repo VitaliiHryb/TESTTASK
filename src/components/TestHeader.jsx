53ec2d3acf6ea2fec2c9d20018b18ae1f0649a0f
VitaliiHryb <vitaliigryb@ukr.net>
2023-07-03 08:37:45 +0300

    refactoring

--- src/components/TestHeader.jsx
+++ src/components/TestHeader.jsx
@@ -40,9 +40,6 @@ const TestHeader = () => {
         className="content-background"
         style={{
           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${backgroundImage})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          color: '#fff',
         }}
       >
         <h2 className="content-background__test-assignment">
